933509f642795598d18b8ca4c1156e36243cd656
AkshayKalamkarr <kalamkarakshay2001@gmail.com>
2025-03-12 11:29:33 +0530

    pratcing

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='bg-red-200 text-2xl font-bold'>Website Running Live</h1>
+      <h1 className='bg-red-400 text-2xl font-bold'>Website Running Live On Server</h1>
     </div>
   );
 }
